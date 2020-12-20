e91f2123500c22c2b5e7783db367b416e19f3d5f
github viplavan <viplava.kilaru@gmail.com>
2020-12-20 15:47:07 -0600

    updated the movie list and added images

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = "Fav Favorite Photos";
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  placeholder = 'https://newcastlebeach.org/images/siva-pictures-7.webp';
+  image3 = 'https://i.pinimg.com/originals/be/bf/d4/bebfd4342f58c6db74e39c3042da8b02.webp';
 
   constructor() { }
 
--- lesson1/exercises/src/app/movie-list/movie-list.component.ts
+++ lesson1/exercises/src/app/movie-list/movie-list.component.ts
@@ -1,16 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-   selector: 'movie-list',
-   templateUrl: './movie-list.component.html',
-   styleUrls: ['./movie-list.component.css']
+  selector: 'movie-list',
+  templateUrl: './movie-list.component.html',
+  styleUrls: ['./movie-list.component.css']
 })
 export class MovieListComponent implements OnInit {
-   movies = ['The Manchurian Candidate', 'Oceans 8'];
+  movies: string[] = ['Bahubali', 'Saaho', 'Srimanthudu', 'Temper'];
 
-   constructor() { }
+  constructor() { }
 
-   ngOnInit() {
-   }
+  ngOnInit() {
+  }
 
 }
